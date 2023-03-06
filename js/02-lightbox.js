fc46506ed6d83dd1c014f70f217f196fe3469593
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-03-06 11:04:53 +0100

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', cardsMarkup);
+gallery.addEventListener('click', onGalleryContainerClick);
+
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+  }).join("");
+};
+
+function onGalleryContainerClick(event) {
+  
+//-Запрети это поведение по умолчанию чтобы браузер не открывал ссылку в новом окне
+  event.preventDefault();
+  
+  var lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+}
